9419092695ee29e1bce393ef9e0706ee0433163e
isaacmg <igodfried@isaac26.com>
2023-11-21 01:17:37 -0500

    run scraping stuff

--- tests/test_usgs.py
+++ tests/test_usgs.py
@@ -1,20 +1,19 @@
 # create unittests for the functions in usgs_scraping_functions.py
 
 from datetime import datetime
-from usgs_scraping_functions import make_usgs_data, process_response_text, df_label, create_csv, column_renamer, rename_cols, create_csv
+from scraping_functions import HydroScraper
 import unittest
+import os
 
 
 class TestUsgsScraping(unittest.TestCase):
     def __setUp__(self):
-        pass
-
-    def test_make_usgs_data(self):
         start_date = datetime(2020, 1, 1)
         end_date = datetime(2020, 1, 1)
-        site_number = "01646500"
-        result = make_usgs_data(start_date, end_date, site_number)
-        self.assertEqual(len(result), 97)
+        self.scraper = HydroScraper(start_date, end_date, os.path.join(os.getcwd(), "test_data", "test_meta.json"))
+
+    def test_make_usgs_data(self):
+        self.assertEqual(len(self.scraper.usgs_df), 97)
 
     def test_col_renamer(self):
         pass
