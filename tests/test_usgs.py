7bc9d5e8cbff9545689dd00837fabf8b8c016fb1
isaacmg <igodfried@isaac26.com>
2023-12-11 02:36:11 -0500

    Revert "fixing orderin 2"
    This reverts commit 6b922ad73a0c31c46eadbce4d1bc7624bb7cb213.

--- tests/test_usgs.py
+++ tests/test_usgs.py
@@ -6,7 +6,7 @@ from weather_scraping_functions import get_snotel_data
 import unittest
 import os
 import pandas as pd
-unittest.TestLoader.sortTestMethodsUsing = None
+
 
 class TestUsgsScraping(unittest.TestCase):
     def setUp(self):
@@ -15,6 +15,7 @@ class TestUsgsScraping(unittest.TestCase):
         self.test_data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_data")
         self.scraper = HydroScraper(start_date, end_date, os.path.join(self.test_data_dir, "test_meta.json"))
         self.western_scraper = HydroScraper(datetime(2021, 12, 1), datetime(2022, 3, 1), os.path.join(self.test_data_dir, "west_meta.json"))
+        unittest.TestLoader.sortTestMethodsUsing = None
 
     def test_asos_data(self):
         self.assertEqual(len(self.scraper.asos_df), 47)  # 47 because we scraped additional day due to time zone issues
